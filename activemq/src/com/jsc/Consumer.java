5e8e5620d23e4ab0dec1dc0baa5670274196f40e
Pasifal_Jsc <Pasifal_jsc@users.noreply.github.com>
2019-05-08 16:07:46 +0800

    消息队列模板

---
+++ activemq/src/com/jsc/Consumer.java
@@ -0,0 +1,52 @@
+package com.jsc;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class Consumer {
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER; // 默认的连接用户名
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD; // 默认的连接密码
+    private static final String BROKEURL = ActiveMQConnection.DEFAULT_BROKER_URL; // 默认的连接地址
+
+    public static void main(String[] args) {
+        System.out.println("USERNAME = " + USERNAME);
+        System.out.println("PASSWORD = " + PASSWORD);
+        System.out.println("BROKEURL = " + BROKEURL);
+
+        Connection connection = null;
+        Session session = null;
+        MessageConsumer consumer;
+
+        try {
+            //创建连接工厂
+            ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKEURL);
+            //创建连接
+            connection = activeMQConnectionFactory.createConnection();
+            //开启连接
+            connection.start();
+            //创建会话session
+            session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+            //创建目标点
+            Destination destination = session.createTopic("雪中粉丝");
+            //创建订阅生产者,生产内容指向destination
+            consumer = session.createConsumer(destination);
+            //创建发送信息
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    TextMessage textMessage=(TextMessage)message;
+                    try {
+                        System.out.println(textMessage.getText());
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            session.commit();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
